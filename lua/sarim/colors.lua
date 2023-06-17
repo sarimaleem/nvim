59c0911aab633eb967e50b857b54b31169c1ce22
Sarim Aleem <sarimaleem99@gmail.com>
2023-06-16 19:25:40 -0700

    updated color scheme

--- lua/sarim/colors.lua
+++ lua/sarim/colors.lua
@@ -2,4 +2,4 @@ vim.g.everforest_background = "hard"
 vim.g.gruvbox_material_background = "hard"
 vim.g.sonokai_style = "atlantis"
 
-vim.cmd("colorscheme gruvbox-material")
+vim.cmd("colorscheme one_monokai")
