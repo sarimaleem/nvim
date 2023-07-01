3a074040d90d9c069dad714be9d73062f060dd6e
Sarim Aleem <sarima@Sarims-MacBook-Pro.local>
2023-06-30 22:27:48 -0500

    fixed markdown preview

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -100,9 +100,7 @@ return require('packer').startup(function(use)
     use { 'kevinhwang91/nvim-bqf', ft = 'qf' }
     use({
         "iamcco/markdown-preview.nvim",
-        run = "cd app && npm install",
-        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
-        ft = { "markdown" },
+        run = function() vim.fn["mkdp#util#install"]() end,
     })
     use "folke/flash.nvim"
     use "Vigemus/iron.nvim"
